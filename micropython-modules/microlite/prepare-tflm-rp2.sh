4165faabd43ab82f1b8658438dbbf04d0cbe0c77
Adam <rodriada000@gmail.com>
2025-02-24 19:16:22 -0500

    test

---
+++ micropython-modules/microlite/prepare-tflm-rp2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# needs to be run from the tensorflow top level directory
+
+
+python3 tensorflow/lite/micro/tools/project_generation/create_tflm_tree.py \
+	--makefile_options="TARGET=cortex_m_generic OPTIMIZED_KERNEL_DIR=cmsis_nn TARGET_ARCH=cortex-m0" \
+       	--rename_cc_to_cpp ../micropython-modules/microlite/tflm
