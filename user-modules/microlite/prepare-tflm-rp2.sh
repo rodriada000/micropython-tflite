a5067352453884b014af663dadeef1bc9e8982d2
Adam <rodriada000@gmail.com>
2025-02-26 19:54:58 -0500

    fix

--- user-modules/microlite/prepare-tflm-rp2.sh
+++ user-modules/microlite/prepare-tflm-rp2.sh
@@ -4,4 +4,4 @@
 
 python3 tensorflow/lite/micro/tools/project_generation/create_tflm_tree.py \
 	--makefile_options="TARGET=cortex_m_generic OPTIMIZED_KERNEL_DIR=cmsis_nn TARGET_ARCH=cortex-m0" \
-       	--rename_cc_to_cpp ../micropython-modules/microlite/tflm
+       	--rename_cc_to_cpp ../user-modules/microlite/tflm
